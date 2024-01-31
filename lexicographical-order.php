38e504905ac2264fef198529d23878a0a5a19b26
Diak Uddin Maulidi <d14k.010@gmail.com>
2024-01-31 23:33:05 +0800

    memperbaiki dan menmbah versi lain

--- lexicographical-order.php
+++ lexicographical-order.php
@@ -1,16 +1,36 @@
 <?php
-
+    // tanpa fungsi array filter
     function inArray($array1, $array2) {
-        $arraynyah = []; 
-        foreach ($array1 as $value1) {
-            if ( array_filter($array2, function($value2) use ($value1) {
-                return strpos($value2, $value1) !== false;
-            } )) {
-                $arraynyah[] = $value1;
+        $result = array();
+        foreach($array1 as $item1){
+            foreach($array2 as $item2){
+                if(strpos($item2,$item1)!==false){
+                    $result [] = $item1;
+                    break;
+                }
             }
         }
-        return $arraynyah;
+        sort($result);
+        return $result;
     }
+
+
+    
+    // // menggunakan fungsi filter
+    // function inArray($array1, $array2) {
+    //     $arraynyah = []; 
+    //     foreach ($array1 as $value1) {
+    //         if ( array_filter($array2, function($value2) use ($value1) {
+    //             return strpos($value2, $value1);
+    //             }) )
+    //         {
+    //             $arraynyah[] = $value1;
+    //         }
+    //     }
+    //     sort($arraynyah);
+    //     return $arraynyah;
+    // }
+
 
     $a1 = ["live", "strong", "tarp"];
     $a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
